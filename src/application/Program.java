4830ca0a2f35bfb0f9848fb80cf82b68384d45d6
Samuel Milani <samuel@talia.nied.unicamp.br>
2023-05-29 16:53:10 -0300

    Seller and Department entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,23 @@
 package application;
 
-import java.sql.Connection;
-import db.DB;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		/*
 		Connection conn = DB.getConnection();
 		DB.closeConnection();
-
+		*/
+		
+		Department obj = new Department(2, "Pac");
+		Seller seller = new Seller(1, "Jeff", "punkjeff@gmail.com", new Date(), 2500.0, obj);
+		
+		System.out.println(seller.toString());
 	}
 
 }
